272354a1c3ef92f51846765d8ec8324b390be4b7
rduong11 <raymondduong12@gmail.com>
2026-05-21 20:16:30 -0500

    setting up for implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/shadcn/button";
+import { Field } from "@/components/ui/shadcn/field";
+import { Input } from "@/components/ui/shadcn/input";
 
 export default function Home() {
   return (
     <div>
       Landing Page
-      <Button>Test</Button>
+      <Field orientation="horizontal">
+        <Input type="vehicle-search" placeholder="AB12345" />
+        <Button>Search</Button>
+      </Field>
     </div>
   );
 }
