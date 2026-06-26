350f4434600475c77b035a0655902f2b74ddead9
rduong11 <raymondduong12@gmail.com>
2026-06-25 23:55:20 -0500

    setting upfor stat bar

---
+++ components/ui/dashboard/StatsBar.tsx
@@ -0,0 +1,10 @@
+export default function StatsBar() {
+  return (
+    <div>
+      <p>Total Vehicles: </p>
+      <p>Pending: </p>
+      <p>In Progress: </p>
+      <p>Ready for pickup: </p>
+    </div>
+  );
+}
